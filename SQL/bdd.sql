45cde29a983e73a7d5cd80fb085f8666b4abf239
Hanlin Wu <hanlin.wu@etu.utc.fr>
2020-05-31 23:24:10 +0200

    Update bdd.sql

---
+++ SQL/bdd.sql
@@ -0,0 +1,25 @@
+-- Suppression des tables et types
+
+DROP TABLE IF EXISTS Batiment
+DROP TABLE IF EXISTS Etage
+DROP TABLE IF EXISTS Salle
+DROP TABLE IF EXISTS PosteTele
+DROP TABLE IF EXISTS Employe
+DROP TABLE IF EXISTS MoyenIT
+DROP TABLE IF EXISTS Machine
+DROP TABLE IF EXISTS Organisation
+DROP TABLE IF EXISTS Laboratoire
+DROP TABLE IF EXISTS Departement
+DROP TABLE IF EXISTS Projet
+DROP TABLE IF EXISTS RHprojet
+DROP TABLE IF EXISTS RessouceProjet_IT
+
+-- Creation des types
+
+CREATE TYPE typetele AS ENUM ('VOIP','TOIP','Landline')
+CREATE TYPE etat AS ENUM ('CDI','CDD','stagiaire')
+CREATE TYPE typeit AS ENUM ('PC','portable','serveur')
+CREATE TYPE gaz AS ENUM ('Butane','methane','oxygene','azote','hydrogene','helium')
+
+-- Creation des tables
+
