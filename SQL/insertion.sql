a60740ed54d7b0d501cd11c2aef1ecb4ceaaecb3
Hanlin Wu <hanlin.wu@etu.utc.fr>
2020-06-02 13:52:45 +0200

    Update insertion.sql

--- SQL/insertion.sql
+++ SQL/insertion.sql
@@ -95,6 +95,53 @@ INSERT INTO Employe VALUES('ca9000ep','bay0002','Mike','Ethan','Methan@gmail.com
 INSERT INTO Employe VALUES('cv1102qw','abw0023','Jade','William','Jwilliam@gmail.com','CDI','labo_02','LSV','CCE');
 INSERT INTO Employe VALUES('jk1034gh','bay0011','Bob','Kevin','Bkevin@hotmail.com','CDD','bureau_a01','LC',NULL);
 
--- Insertion des 
+-- Insertion des machines :
 
--- Insertion des
+INSERT INTO Machine(codeMach,type,sallemach,description,puissanceElec,besoinTriphase,besoinResaux,besoinGaz,taille,NCM) VALUES('IMP1-1','Mach_fabr','aaa0001','impfichier','80','0','1',NULL,'12','#wg1208');
+INSERT INTO Machine(codeMach,type,sallemach,description,puissanceElec,besoinTriphase,besoinResaux,besoinGaz,taille,NCM) VALUES('ACR2-1','Mach_labo','abw0031','air_test','200','1','0','oxygene','30','#ad5012');
+INSERT INTO Machine(codeMach,type,sallemach,description,puissanceElec,besoinTriphase,besoinResaux,besoinGaz,taille,NCM) VALUES('SDR2-2','Mach_fabr','abw0004','synthèse de réactifs','500','1','1',NULL,'50','#sf1007');
+INSERT INTO Machine(codeMach,type,sallemach,description,puissanceElec,besoinTriphase,besoinResaux,besoinGaz,taille,NCM) VALUES('MCE2-1','Mach_labo','abw0023','Microscope électronique','100','1','1',NULL,'20','#me2137');
+INSERT INTO Machine(codeMach,type,sallemach,description,puissanceElec,besoinTriphase,besoinResaux,besoinGaz,taille,NCM) VALUES('MEM1-2','Mach_fabr','bay0011','Machine d embouteillage','200','1','0',NULL,'30','#em8113');
+
+   INSERT INTO Num_Entreprise_Mach VALUES('#wg1208','BPIfrance');
+   INSERT INTO Num_Entreprise_Mach VALUES('#ad5012','Fluke');
+   INSERT INTO Num_Entreprise_Mach VALUES('#sf1007','MECATHEIL');
+   INSERT INTO Num_Entreprise_Mach VALUES('#me2137','Fluke');
+   INSERT INTO Num_Entreprise_Mach VALUES('#em8113','MECATHEIL');
+
+-- Insertion des moyens informatiques :
+
+INSERT INTO MoyenIT VALUES('pcf1000','aaa0015',NULL,'aa1000ez','PC','Windows10');
+INSERT INTO MoyenIT VALUES('por1001','abw0004','MCE2-1','bh2019qz','portable','apple');
+INSERT INTO MoyenIT VALUES('pcs1000','aaa0023',NULL,'ac0120ef','serveur','Linux');
+INSERT INTO MoyenIT VALUES('pcf1012','abw0004','SDR2-2','bh2019qz','PC','Mac');
+INSERT INTO MoyenIT VALUES('por2015','bay0000',NULL,NULL,'portable','apple');
+
+   INSERT INTO AddressMoyenIT VALUES('pcf1000','5E:FF:56:A2:AF:15');
+   INSERT INTO AddressMoyenIT VALUES('pcf1000','3A:FF:62:B3:DE:23');
+   INSERT INTO AddressMoyenIT VALUES('por1001','4F:FF:12:C6:AC:36');
+   INSERT INTO AddressMoyenIT VALUES('pcs1000','8E:FF:17:B4:AD:18');
+   INSERT INTO AddressMoyenIT VALUES('pcf1012','1E:FF:47:E2:CF:39');
+   INSERT INTO AddressMoyenIT VALUES('pcf1012','2D:FF:36:D2:AE:67');
+   INSERT INTO AddressMoyenIT VALUES('por2015','3B:FF:45:C7:AB:26');
+
+-- Insertion des postes telephoniques :
+
+INSERT INTO PosteTele(numInt,numExt,salletele,hostID,typeTele,marque) VALUES('100076','3673217','aaa0000',NULL,'VOIP','Consultation à la réception');
+INSERT INTO PosteTele(numInt,numExt,salletele,hostID,typeTele,marque) VALUES('100703','2087691','aaa0023','ac0120ef','TOIP','xxx');
+INSERT INTO PosteTele(numInt,numExt,salletele,hostID,typeTele,marque) VALUES('100112','4294627','bay0000','as1900oz','Landline','xxxxx');
+INSERT INTO PosteTele(numInt,numExt,salletele,hostID,typeTele,marque) VALUES('101026','2763558','abw0031',NULL,'VOIP','xxxxxxxxx');
+
+-- Insertion des relations entre les projets et les employes :
+
+INSERT INTO RHprojet VALUES('IEQNP','ad0401ez','1',NULL);
+INSERT INTO RHprojet VALUES('IEQNP','ac0120ef','0','Estimateur');
+INSERT INTO RHprojet VALUES('DNCV','jk1034gh','1',NULL);
+INSERT INTO RHprojet VALUES('DNCV','bh2019qz','0','R&D');
+INSERT INTO RHprojet VALUES('DNCV','ad0401ez','0','testing');
+
+-- Insertion des relations rntre les projets et les moyens informatiques :
+
+INSERT INTO RessouceProjet_IT VALUES('DNCV','por1001');
+INSERT INTO RessouceProjet_IT VALUES('DNCV','pcf1012');
+INSERT INTO RessouceProjet_IT VALUES('EMMPR','por2015');
