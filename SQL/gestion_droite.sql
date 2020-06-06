9a4d21dabce730bf8e7fd43f5f14a1566997f5fe
Hanlin Wu <hanlin.wu@etu.utc.fr>
2020-06-06 12:35:31 +0200

    Update gestion_droite.sql

--- SQL/gestion_droite.sql
+++ SQL/gestion_droite.sql
@@ -2,9 +2,9 @@ CREATE USER Admin;
 
 CREATE USER RH;
 
-CREATE USER Directeur;
+CREATE GROUP Directeur WITH USER d1,d2;
 
-CREATE USER Chef;
+CREATE GROUP Chef WITH USER c1,c2;
 
 GRANT ALL PRIVILEGES ON Batiment,Etage,Salle,GazSpecifique,PhotoSalle,Employe,PosteTele,MoyenIT,AddressMoyenIT,Num_Entreprise_Mach,Machine,Organisation,Laboratoire,Departement,Projet,RHprojet,RessouceProjet_IT TO Admin;
 
@@ -13,3 +13,5 @@ GRANT INSERT,SELECT,UPDATE ON Laboratoire,Departement,RHprojet,Employe TO RH;
 GRANT INSERT,SELECT,UPDATE ON Employe,RHprojet TO Directeur;
 
 GRANT INSERT,SELECT,UPDATE ON RHprojet,RessouceProjet_IT TO Chef;
+
+GRANT SELECT ANY TABLE TO Public; 
